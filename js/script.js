e4f4246644e5c0f454377e3a5aaa8c0b8359c4e8
rosonhuang <1214036933@qq.com>
2017-04-30 15:34:32 -0700

    logout 功能

--- js/script.js
+++ js/script.js
@@ -28,4 +28,32 @@ $(document).ready(function() {
         }
 
     });
+
+    $("#loginBtn").click(function(event) {
+        event.preventDefault();
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        if (username && password) {
+            $.post("http://open-commerce.herokuapp.com/api/login", {
+                    username: username,
+                    password: password
+                },
+                function(response) {
+                    if (response.success) {
+                        var cookie = "x-access-token=" + response.token;
+                        document.cookie = cookie;
+                        window.location.href = "/index.html";
+                    } else {
+                        alert(response.message);
+                    }
+
+                });
+
+        } else {
+            alert("please provide a username and password for login!");
+
+        }
+    });
 });
