ba8de7a2d7bacef5f93d3749e8af91f5cc1bd287
rosonhuang <1214036933@qq.com>
2017-04-30 15:02:37 -0700

    001

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $("#signupBtn").click(function(event) {
+        console.log(event);
+        // alert("sign up btn clicked")
+        event.preventDefault();
+
+        var username = $("#username").val();
+        var password = $("#password").val();
+        // console.log(username + " " + password);
+
+        if (username && password) {
+            $.post("http://open-commerce.herokuapp.com/api/signup", {
+                    username: username,
+                    password: password
+                },
+                function(response) {
+                    if (response.success) {
+                        alert("signup successful!");
+
+                    } else {
+                        alert(response.message);
+                    }
+                });
+
+        } else {
+            alert("please provide a username and password for signup!");
+        }
+
+    });
+});
